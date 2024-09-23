454c3636bc4f8c95ac5582ba6813204df3a67169
Maksim Levanovich <maxlevanovich@gmail.com>
2024-09-23 09:43:01 -0500

    pulled Bogdan's JS

---
+++ Simon Game/app.js
@@ -0,0 +1,24 @@
+let one = document.querySelectorAll(".one")
+let two = document.querySelectorAll(".two")
+let three = document.querySelectorAll(".three")
+let four = document.querySelectorAll(".four")
+
+
+// $(".square").on("click", function () {
+//     $("header").text()
+//   $(".header").toggle().fadeOut().fadeIn().text("To start level press A").animate({opacity: 0.5}) 
+
+// })
+
+let countLevels = 10
+
+function switchLevel(){
+    let count = 0;
+    $(".header").on("click", function () {
+            $(".header").text(`Level ${count}`);
+            count++;
+             })    
+        }
+
+
+
