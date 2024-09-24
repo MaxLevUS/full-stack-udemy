afb27f70906bdbd5ce7da8b5b372724716d90828
Maksim Levanovich <maxlevanovich@gmail.com>
2024-09-23 21:42:16 -0500

    added

--- Simon Game/app.js
+++ Simon Game/app.js
@@ -1,7 +1,26 @@
-let one = document.querySelectorAll(".one")
-let two = document.querySelectorAll(".two")
-let three = document.querySelectorAll(".three")
-let four = document.querySelectorAll(".four")
+let one = document.querySelectorAll("#one")
+let two = document.querySelectorAll("#two")
+let three = document.querySelectorAll("#three")
+let four = document.querySelectorAll("#four")
+let squares = document.querySelectorAll(".square").length;
+
+
+document.querySelectorAll(".square").forEach((el) => {
+    el.addEventListener("click", respondFunction);
+});
+
+function respondFunction() {
+    alert("Clicked");
+}
+
+
+// for(let i=0; i<squares;i++){
+//     document.querySelectorAll(".square")[i].addEventListener("click", function(){
+//         alert("I've got clicked")
+//     })
+// }
+
+
 
 
 // $(".square").on("click", function () {
@@ -10,14 +29,16 @@ let four = document.querySelectorAll(".four")
 
 // })
 
-let countLevels = 10
+
+    // let count = 0;
+    // $(".header").on("keypress", function () {
+    //         $(".header").text(`Level ${count}`);
+    //         count++;
+    //          })   
 
 
-    let count = 0;
-    $(".header").on("click", function () {
-            $(".header").text(`Level ${count}`);
-            count++;
-             })    
+
+
 
 
 
